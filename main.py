88e8e1f5771af4397a0b0202d1d8f52bb999f1b7
potatorobot26 <ronbrodsky.yolovich@gmail.com>
2026-06-27 15:16:24 +0300

    still not working

---
+++ main.py
@@ -0,0 +1,56 @@
+import sys
+
+#reads arguments 
+program_filepath = sys.argv[1]
+
+# read file lines 
+program_lines = []
+with open(program_filepath, "r") as program_file:
+    program_lines = [line.strip() for line in program_file.readlines()]
+
+program = []
+token_counter = 0
+label_tracker = {}
+for line in program_lines:
+    parts = line.split(" ")
+    opcode = parts[0]
+
+    # check if line is empty
+    if opcode =="":
+        continue
+    # check if label
+    if opcode.endswith(":"):
+        label_tracker[opcode[:-1]] = token_counter
+        continue
+    #store opcode data
+    program.append(opcode)
+    token_counter += 1
+
+    # handle each opcode
+    if opcode == "PUSH":
+        # expecting a number
+        number = int(parts[1])
+        program.append(number)
+        token_counter += 1
+    elif opcode == "PRINT":
+        # parse string literal
+        string_literal = ' '.join(parts[1:])[1:-1]
+        program.append(string_literal)
+        token_counter += 1
+    elif opcode == "JUMP.EQ.0":
+        # read a label
+        label = parts[1]
+        program.append(label)
+        token_counter += 1 
+
+    elif opcode == "JUMP.GT.0":
+        # read a label
+        label = parts[1]
+        program.append(label)
+        token_counter += 1 
+
+
+print(program)
+
+class Stack:
+    
